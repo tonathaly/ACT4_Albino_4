fc5f3703942782448eb04bcaac93a146c864f66b
tonathaly <nathalyalbino.029@gmail.com>
2023-11-14 00:45:39 +0800

    my activity 4.4 commit

---
+++ src/main/java/com/mycompany/act4_albino_4/ACT4_Albino_4.java
@@ -0,0 +1,18 @@
+package com.mycompany.act4_albino_4;
+
+public class ACT4_Albino_4 {
+
+    public static void main(String[] args) {
+        System.out.println("x                       x");
+        System.out.println("x                       x");
+        System.out.println("x      xxxxxxxxxx       x");
+        System.out.println("xxxxx  x        x   xxxxx");
+        System.out.println("x   x  x        x   x   x");
+        System.out.println("x   x  x        x   x   x");
+        
+        
+        
+        
+        
+    }
+}
